d65d584cb5038a92267acf8cf4dd9263c9f59180
GustavoAco <33663818+GustavoAco@users.noreply.github.com>
2018-05-16 15:01:18 -0500

    Update run.sh

--- sparkml/run.sh
+++ sparkml/run.sh
@@ -2,13 +2,13 @@
 
 export SPARK_DIST_CLASSPATH=$(hadoop classpath)
 
-SPARK_CLASS=com.cloudera.sdk.SparkMLExample
-SPARK_BUILD_DIR=target/scala-2.11
-SPARK_JAR=sparkmlexample_2.11-1.0.jar
-SPARK_BASE=input/
+SPARK_CLASS=SPARK
+SPARK_BUILD_DIR=sparkml
+SPARK_JAR=spark_2.10-0.1.jar
+SPARK_BASE=../admin/input
 ITERS=100
 
-DEPLOY_MODE=xxx
-MASTER=xxx
+DEPLOY_MODE=cluster
+MASTER=yarn
 
 spark2-submit --deploy-mode ${DEPLOY_MODE} --master ${MASTER} --executor-memory 1G --num-executors 2 --executor-cores 1 --class ${SPARK_CLASS} ${SPARK_BUILD_DIR}/${SPARK_JAR} ${SPARK_BASE} ${ITERS}
