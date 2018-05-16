8e738fdb48153711c2026bae33d3e8f31fe903d0
GustavoAco <33663818+GustavoAco@users.noreply.github.com>
2018-05-15 23:33:45 -0500

    Update SparkBatchExample.scala

--- spark/src/main/scala/com/cloudera/sdk/SparkBatchExample.scala
+++ spark/src/main/scala/com/cloudera/sdk/SparkBatchExample.scala
@@ -1,27 +1,37 @@
-package com.cloudera.sdk
-
-import org.apache.spark.{SparkConf, SparkContext }
+import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.rdd.RDD
-
-object SparkBatchExample {
-    def main(args: Array[String]): Unit = {
-        if (args.length < 1) {
-            System.err.println("Usage: SparkBatchExample <path>")
-            System.exit(1)
-        }
-
-        val path = args(0)
-        val conf = new SparkConf()
-            .setAppName("Spark Batch Example")
-            .setMaster("local[2]")
-            .set("spark.io.compression.codec", "lz4")
-        val sc = new SparkContext(conf)
- 
-        conf.set("spark.cleaner.ttl", "120000")
-
-        /* READ DATA FROM HDFS, SORT BY KEY, SAVE TO HDFS */
+object SPARK{
+  def main(args: Array[String]): Unit = {
+    if (args.length < 1) {
+      System.err.println("Usage: SparkBatchExample <path>")
+      System.exit(1)
     }
-}
 
+    val path = args(0)
+    val conf = new SparkConf()
+      .setAppName("Spark Batch Example")
+      .setMaster("local[2]")
+      .set("spark.io.compression.codec", "lz4")
+    val sc = new SparkContext(conf)
+    conf.set("spark.cleaner.ttl", "120000")
+    //println("*****"*100)
+    //println("************READ FILE *****************")
+    //println("*****"*100)
+    //println(args(0))
+    val df=sc.textFile("hdfs://clouderan2.ricohits.ga"+path)
+    val data =df.map(line => line.split(",")).map(arr => (arr(0), arr(1)))
+    //println("*****"*100)
+    //println("************ORDENO*****************")
+    //println("*****"*100)
+    //val orden=data.sortByKey()
+    val orden=data.coalesce(1).sortByKey()
+    orden.saveAsTextFile(path+"SPARK")
+
+    //println("********"*100)
+
+
+  }
+
+}
