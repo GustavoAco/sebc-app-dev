28e1b3a4a4ace04a2a9ff7328abe0fd2750f4bad
GustavoAco <33663818+GustavoAco@users.noreply.github.com>
2018-05-17 09:22:29 -0500

    Update run.sh

--- sparkstreaming/flume/run.sh
+++ sparkstreaming/flume/run.sh
@@ -2,13 +2,13 @@
 
 export SPARK_DIST_CLASSPATH=$(hadoop classpath)
 
-SPARK_CLASS=com.cloudera.sdk.SparkStreamExample
-SPARK_BUILD_DIR=target/scala-2.11
+SPARK_CLASS=SPARK
+SPARK_BUILD_DIR=sparkstreaming
 SPARK_JAR=sparkstreamexample_2.11-1.0.jar
-HOST=0.0.0.0
+HOST=172.21.14.108
 PORT=41415
 
-DEPLOY_MODE=xxx
-MASTER=xxx
+DEPLOY_MODE=cluster
+MASTER=yarn
 
 spark2-submit --deploy-mode ${DEPLOY_MODE} --master ${MASTER} --executor-memory 1G --num-executors 4 --executor-cores 2 --class ${SPARK_CLASS} ${SPARK_BUILD_DIR}/${SPARK_JAR} ${HOST} ${PORT}
